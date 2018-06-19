0c62e820bb48016c43f0c07be620dd4a2c74e3ce
jack <1169392456@qq.com>
2018-06-19 18:23:39 +0800

    Initial commit

---
+++ main/java/example/One.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class One {
+    String message = "foo";
+
+    public String foo() {
+        return message;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println("uu");
+    }
+}
